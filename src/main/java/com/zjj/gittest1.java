faebecfa6bf0cc50620e446e18dde050598a4644
zhang <zhang@zhang.com>
2022-08-31 09:40:14 +0800

    hot-fix first commit

--- src/main/java/com/zjj/gittest1.java
+++ src/main/java/com/zjj/gittest1.java
@@ -3,5 +3,6 @@ package com.zjj;
 public class gittest1 {
     public static void main(String[] args) {
         System.out.println("master frist commit!");
+        System.out.println("hot-fix frist commit");
     }
 }
