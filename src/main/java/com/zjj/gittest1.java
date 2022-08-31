f81c0c7c4a622c5acd6b3ee2aab350cfbc55f366
zhang <zhang@zhang.com>
2022-08-31 10:26:14 +0800

    push first commit

--- src/main/java/com/zjj/gittest1.java
+++ src/main/java/com/zjj/gittest1.java
@@ -4,5 +4,6 @@ public class gittest1 {
     public static void main(String[] args) {
         System.out.println("master frist commit!");
         System.out.println("hot-fix frist commit");
+        System.out.println("push test");
     }
 }
